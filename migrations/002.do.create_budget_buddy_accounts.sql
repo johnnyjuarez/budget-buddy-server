879001f2ca276922a19cf71397ec1087e6516eba
johnnyjuarez <johnnyjuarez1992@gmail.com>
2020-11-08 15:02:21 -0600

    added migrations

---
+++ migrations/002.do.create_budget_buddy_accounts.sql
@@ -0,0 +1,7 @@
+CREATE TABLE budget_buddy_accounts(
+  id SERIAL PRIMARY KEY,
+  account_name TEXT NOT NULL,
+  account_total INTEGER,
+  user_id INTEGER 
+    REFERENCES budget_buddy_users(id) ON DELETE CASCADE NOT NULL
+);
---
+++ migrations/003.do.create_budget_buddy_transactions.sql
@@ -0,0 +1,8 @@
+CREATE TABLE budget_buddy_transactions (
+  id SERIAL PRIMARY KEY,
+  type TEXT NOT NULL,
+  date_added TIMESTAMPTZ DEFAULT now() NOT NULL,
+  description TEXT,
+  account_id INTEGER 
+    REFERENCES budget_buddy_accounts(id) ON DELETE CASCADE NOT NULL
+);
